d9b3a77d2e55566fe1a19839bd147debfb204b88
Sam Craig <craigsamm@gmail.com>
2019-08-12 00:06:26 -0500

    Add future data note

--- models/season.js
+++ models/season.js
@@ -1,6 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Season = sequelize.define('Season', {
+    // This will need to have ID not be primary key eventually, to allow
+    // for multiple saves.
     year: {
       type: DataTypes.INTEGER,
       primaryKey: true
