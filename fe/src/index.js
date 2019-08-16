996c8d6df84927f89b78e091e2a1ab0a3cd89101
Sam Craig <craigsamm@gmail.com>
2019-08-16 08:48:43 -0500

    Add quick routing

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
+import Schedule from './Schedule';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Not sure if App will actually be used later on.
+// Maybe implement later parts of https://codeburst.io/getting-started-with-react-router-5c978f70df91
+const routing = (
+  <Router>
+    <Route exact path="/" component={App} />
+    <Route path="/schedule" component={Schedule} />
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
