4b4f5b174027fd6150cf09180d4814fb027ba832
Sam Craig <craigsamm@gmail.com>
2019-08-18 07:55:19 -0500

    Open new windows for new processes

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-nodemon api/server.js &
-yarn --cwd "fe" start &
+osascript -e "tell application \"Terminal\" to do script \"cd $(pwd); nodemon --watch api api/server.js\""
+osascript -e "tell application \"Terminal\" to do script \"cd $(pwd); REACT_APP_API_URL=localhost:3002 yarn --cwd fe start\""
