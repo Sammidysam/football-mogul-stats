c666d190b8165f01e1fea7358262dd292246cf21
Sam Craig <craigsamm@gmail.com>
2019-08-20 08:22:00 -0500

    Add primary key to TeamParticipation table

--- models/teamparticipation.js
+++ models/teamparticipation.js
@@ -1,6 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const TeamParticipation = sequelize.define('TeamParticipation', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     // better to be an enum to allow for neutral site
     home: DataTypes.BOOLEAN,
     score: DataTypes.INTEGER
