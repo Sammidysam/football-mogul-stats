9826ea0bf49b807e351ea301c60eaf596c488c66
Sam Craig <craigsamm@gmail.com>
2019-08-21 08:07:02 -0500

    Style a game listing

--- fe/src/components/GameList.js
+++ fe/src/components/GameList.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import { withStyles } from '@material-ui/styles';
+
 const api = require('../api.js');
+
+const styles = {
+  paper: {
+    margin: [[5, 0]]
+  }
+};
 
 class GameList extends React.Component {
   constructor(props) {
@@ -32,29 +44,52 @@ class GameList extends React.Component {
         })
         .then(teamParticipations => this.setState({ teamParticipations: teamParticipations }));
       });
-
-      api.fetch('teams')
-      .then(teams => this.setState({ teams: teams }));
     }
+  }
+
+  componentDidMount() {
+    api.fetch('teams')
+    .then(teams => this.setState({ teams: teams }));
   }
 
   render() {
     const { games, teams, teamParticipations } = this.state;
-    const { playoff, week, season } = this.props;
+    const { playoff, week, season, classes } = this.props;
 
-    return games.map(g => {
-      const away = teamParticipations.find(tp => tp.GameId === g.id && !tp.home) || {};
-      const awayTeam = teams.find(t => t.id === away.TeamId) || {};
-      const home = teamParticipations.find(tp => tp.GameId === g.id && tp.home) || {};
-      const homeTeam = teams.find(t => t.id === home.TeamId) || {};
+    return (
+      <Box display="flex" flexDirection="column" width="80%">
+        {games.map(g => {
+          const away = teamParticipations.find(tp => tp.GameId === g.id && !tp.home) || {};
+          const awayTeam = teams.find(t => t.id === away.TeamId) || {};
+          const home = teamParticipations.find(tp => tp.GameId === g.id && tp.home) || {};
+          const homeTeam = teams.find(t => t.id === home.TeamId) || {};
 
-      return (
-        <div key={g.id}>
-          {awayTeam.name} {away.score} - {homeTeam.name} {home.score}
-        </div>
-      );
-    });
+          const winnerId = away.score > home.score ? away.id : home.id;
+
+          return (
+            <Paper display="flex" flexDirection="column" className={classes.paper} key={g.id}>
+              <Box padding="5px">
+                <Box>
+                  <Typography>
+                    <Box fontWeight={winnerId === away.id ? 'fontWeightMedium' : 'fontWeightRegular'}>
+                      {awayTeam.name} {away.score}
+                    </Box>
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography>
+                    <Box fontWeight={winnerId === home.id ? 'fontWeightMedium' : 'fontWeightRegular'}>
+                      {homeTeam.name} {home.score}
+                    </Box>
+                  </Typography>
+                </Box>
+              </Box>
+            </Paper>
+          );
+        })}
+      </Box>
+    );
   }
 }
 
-export default GameList;
+export default withStyles(styles)(GameList);
