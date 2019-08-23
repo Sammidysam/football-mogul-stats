20897ccf731f201f642d47662a0ce4931036aeb6
Sam Craig <craigsamm@gmail.com>
2019-08-23 08:00:56 -0500

    Genericize link helper function

--- scraper/divisionsconferences.js
+++ scraper/divisionsconferences.js
@@ -1,5 +1,7 @@
 const models = require('../models');
 
+const linkHelper = require('./helpers/links.js');
+
 // I've seen that 1959 is starting to be supported later, so this may change.
 const SUPER_BOWL_WEEK = 4;
 
@@ -76,66 +78,12 @@ const createGroupingLink = (game, awayTeam, homeTeam, season) => {
   }
 };
 
-// Genericize this and resolveConferenceLinks.
-const resolveDivisionLinks = divisionLinks => (
-  divisionLinks.reduce((total, currentValue) => {
-    // All division links should be 'same' unless something weird is going on
-    // (like the Jets bug).
-    // If so, we skip it.
-    if (currentValue.type === 'different')
-      return total;
-
-    const teamIds = currentValue.teamIds;
-    const indexes = teamIds.map(tid => total.findIndex(d => d.has(tid)));
-    const greatestIndex = Math.max(...indexes);
-
-    if (greatestIndex === -1) {
-      total.push(new Set(teamIds));
-    } else if (indexes.every(i => i > -1) && (new Set(indexes)).size > 1) {
-      // Merge the two groups.
-      const removed = total[indexes[0]];
-
-      teamIds.forEach(tid => total[indexes[1]].add(tid));
-      total.splice(indexes[0], 1);
-    } else {
-      teamIds.forEach(tid => total[greatestIndex].add(tid));
-    }
-
-    return total;
-  }, [])
-);
-
-const resolveConferenceLinks = (conferenceLinks, divisions) => (
-  conferenceLinks.reduce((total, currentValue) => {
-    if (currentValue.type === 'different')
-      return total;
-
-    const divisionIndexes = currentValue.teamIds.map(tid => divisions.findIndex(d => d.has(tid)));
-    const indexes = divisionIndexes.map(tid => total.findIndex(c => c.has(tid)));
-    const greatestIndex = Math.max(...indexes);
-
-    if (greatestIndex === -1) {
-      total.push(new Set(divisionIndexes));
-    } else if (indexes.every(i => i > -1) && (new Set(indexes)).size > 1) {
-      // Merge the two groups.
-      const removed = total[indexes[0]];
-
-      divisionIndexes.forEach(di => total[indexes[1]].add(di));
-      total.splice(indexes[0], 1);
-    } else {
-      divisionIndexes.forEach(di => total[greatestIndex].add(di));
-    }
-
-    return total;
-  }, [])
-);
-
 const resolveGroupingLinks = links => {
   const divisionLinks = links.filter(r => r.object === 'Division');
   const conferenceLinks = links.filter(r => r.object === 'Conference');
 
-  const divisions = resolveDivisionLinks(divisionLinks);
-  const conferences = resolveConferenceLinks(conferenceLinks, divisions);
+  const divisions = linkHelper.linksToGroups(divisionLinks);
+  const conferences = linkHelper.linksToGroups(conferenceLinks, divisions);
 
   console.log(divisions);
   console.log(conferences);
---
+++ scraper/helpers/links.js
@@ -0,0 +1,34 @@
+// Accepts link objects from `divisionsconferences.js`
+// indexObject adds support for mapping teams to divisons then grouping them.
+const linksToGroups = (links, indexObject) => (
+  links.reduce((total, currentValue) => {
+    // All links should be 'same' unless something weird is going on
+    // (like the Jets/Giants bug).
+    // If so, we skip it.
+    if (currentValue.type === 'different')
+      return total;
+
+    const teamIds = currentValue.teamIds;
+    const warpedIndexes = indexObject ? teamIds.map(tid => indexObject.findIndex(d => d.has(tid))) : teamIds;
+    const indexes = warpedIndexes.map(tid => total.findIndex(d => d.has(tid)));
+    const greatestIndex = Math.max(...indexes);
+
+    if (greatestIndex === -1) {
+      total.push(new Set(warpedIndexes));
+    } else if (indexes.every(i => i > -1) && (new Set(indexes)).size > 1) {
+      // Merge the two groups.
+      const removed = total[indexes[0]];
+
+      warpedIndexes.forEach(tid => total[indexes[1]].add(tid));
+      total.splice(indexes[0], 1);
+    } else {
+      warpedIndexes.forEach(tid => total[greatestIndex].add(tid));
+    }
+
+    return total;
+  }, [])
+);
+
+module.exports = {
+  linksToGroups
+};
