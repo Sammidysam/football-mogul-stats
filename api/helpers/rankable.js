23b0093b834f300ab304d03927a3e6ade5138815
Sam Craig <craigsamm@gmail.com>
2020-06-06 10:42:58 -0400

    Provide helper to remove repetition -> apply to rankings

---
+++ api/helpers/rankable.js
@@ -0,0 +1,56 @@
+const models = require('../../models');
+
+const produceTeamData = (req, reduceFunc, reduceObj) => {
+  const seasonQuery = Promise.resolve((
+    req.query.year ?
+      models.Season.findAll({ where: { year: req.query.year } }) :
+      false
+  ));
+
+  return seasonQuery
+  .then(seasons => (
+    models.Team.findAll({})
+    .then(teams => {
+      const promises = teams.map(team => (
+        models.TeamParticipation.findAll({
+          where: {
+            TeamId: team.id
+          },
+          include: [{
+            model: models.Game,
+            ...(seasons && { where: {
+              SeasonYear: seasons.map(s => s.year)
+            }})
+          }]
+        })
+        .then(teamParticipations => (
+          models.TeamParticipation.findAll({
+            where: {
+              GameId: teamParticipations.map(tp => tp.Game.id),
+              TeamId: {
+                [models.Sequelize.Op.not]: team.id
+              }
+            }
+          })
+          .then(others => (
+            {
+              TeamId: team.id,
+              DivisionId: team.DivisionId,
+              ...others.reduce(
+                (total, currentValue) => reduceFunc(total, currentValue, teamParticipations),
+                // Pesky way of cloning an object.
+                JSON.parse(JSON.stringify(reduceObj))
+              )
+            }
+          ))
+        ))
+      ));
+
+      return Promise.all(promises);
+    })
+  ));
+};
+
+module.exports = {
+  produceTeamData
+};
--- api/rankings.js
+++ api/rankings.js
@@ -1,85 +1,46 @@
 const express = require('express');
 const router = express.Router();
 
-const models = require('../models');
+const rankable = require('./helpers/rankable.js');
 
-// Started copied from standings.js - find overlap and remove patterns?
-router.get('/', (req, res) => {
-  const seasonQuery = Promise.resolve((
-    req.query.year ?
-      models.Season.findAll({ where: { year: req.query.year } }) :
-      false
-  ));
+router.get('/', (req, res) => (
+  rankable.produceTeamData(
+    req,
+    (total, currentValue, teamParticipations) => {
+      const ours = teamParticipations.find(tp => tp.GameId === currentValue.GameId);
+      const toAdd = ours.Game.playoff ? total.postSeason : total.regularSeason;
 
-  seasonQuery
-  .then(seasons => (
-    models.Team.findAll({})
-    .then(teams => {
-      const promises = teams.map(team => (
-        models.TeamParticipation.findAll({
-          where: {
-            TeamId: team.id
-          },
-          include: [{
-            model: models.Game,
-            ...(seasons && { where: {
-              SeasonYear: seasons.map(s => s.year)
-            }})
-          }]
-        })
-        .then(teamParticipations => (
-          models.TeamParticipation.findAll({
-            where: {
-              GameId: teamParticipations.map(tp => tp.Game.id),
-              TeamId: {
-                [models.Sequelize.Op.not]: team.id
-              }
-            }
-          })
-          .then(others => (
-            {
-              TeamId: team.id,
-              DivisionId: team.DivisionId,
-              ...others.reduce((total, currentValue) => {
-                const ours = teamParticipations.find(tp => tp.GameId === currentValue.GameId);
-                const toAdd = ours.Game.playoff ? total.postSeason : total.regularSeason;
+      toAdd.offense += ours.rushingYards + ours.passingYards;
+      toAdd.defense += currentValue.rushingYards + currentValue.passingYards;
+      toAdd.opponentIds.push(currentValue.TeamId);
 
-                toAdd.offense += ours.rushingYards + ours.passingYards;
-                toAdd.defense += currentValue.rushingYards + currentValue.passingYards;
-                toAdd.opponentIds.push(currentValue.TeamId);
+      return total;
+    },
+    {
+      regularSeason: {
+        offense: 0,
+        defense: 0,
+        opponentIds: []
+      },
+      postSeason: {
+        offense: 0,
+        defense: 0,
+        opponentIds: []
+      }
+    }
+  )
+  .then(result => {
+    const sorted = result.sort((a, b) => b.regularSeason.offense - a.regularSeason.offense);
+    sorted.forEach((t, index) => t.ranking = index + 1);
 
-                return total;
-              }, {
-                regularSeason: {
-                  offense: 0,
-                  defense: 0,
-                  opponentIds: []
-                },
-                postSeason: {
-                  offense: 0,
-                  defense: 0,
-                  opponentIds: []
-                }
-              })
-            }
-          ))
-        ))
-      ));
+    sorted.forEach(t => {
+      t.regularSeason.opponentIds = t.regularSeason.opponentIds.map(oid => ({
+        [oid]: sorted.find(it => it.TeamId === oid).ranking
+      }))
+    });
 
-      Promise.all(promises).then(result => {
-        const sorted = result.sort((a, b) => b.regularSeason.offense - a.regularSeason.offense);
-        sorted.forEach((t, index) => t.ranking = index + 1);
-
-        sorted.forEach(t => {
-          t.regularSeason.opponentIds = t.regularSeason.opponentIds.map(oid => ({
-            [oid]: sorted.find(it => it.TeamId === oid).ranking
-          }))
-        });
-
-        res.json(sorted);
-      });
-    })
-  ));
-});
+    res.json(result);
+  })
+));
 
 module.exports = router;
